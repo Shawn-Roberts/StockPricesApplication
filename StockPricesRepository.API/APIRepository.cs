b9da8842ad8147fdded30bc51fd24ac4c8848633
Shawn Roberts <Shawn.Roberts@grey.ca>
2020-01-12 10:17:36 -0500

    More work

---
+++ StockPricesRepository.API/APIRepository.cs
@@ -0,0 +1,11 @@
+﻿using StockPricesRepository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockPricesRepository.API
+{
+    public class APIRepository: IStockPricesRepository
+    {
+    }
+}
